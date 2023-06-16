f0731e6102f0fef5ff76094aeafd25e63975af91
shrinemtg <shrine.mtg@gmail.com>
2023-06-17 08:31:24 +0900

    ここを基盤に進めていきます

--- libs/client.js
+++ libs/client.js
@@ -2,5 +2,7 @@ import { createClient } from "microcms-js-sdk";
 
 export const client = createClient({
   serviceDomain: "train-blog",
-  apiKey: process.env.API_KEY,
+  // TODO APIキーの取得の方法確認（envファイルを使う場合）
+  // apiKey: process.env.API_KEY,
+  apiKey: "mQezLvzfWQ6UC26qcGZaPPrRDFcGjNk0oJNe",
 });
--- src/app/Main/main.tsx
+++ src/app/Main/main.tsx
@@ -1,29 +1,81 @@
-"use client";
+// "use client";
+// clientのインポート
+import { client } from "../../../libs/client";
+// import React from "react";
 // import styled from "styled-components";
-import React from "react";
 // import Image from "next/image";
 // import Link from "next/link";
-import { client } from "../../../libs/client";
+import parse from "html-react-parser";
 
-// SSG
-export const getstaticProps = async () => {
-  const data = await client.get({ endpoint: "blog" });
-  console.log(data);
-  return {
-    props: {
-      blog: data.contents,
-    },
-  };
+type Blogs = {
+  contents: [
+    {
+      body: string;
+      createdAt: string;
+      // id: string;
+      // publishedAt: string;
+      // revisedAt: string;
+      title: string;
+      // updatedAt: string;
+    }
+  ];
+  totalCount: number;
+  offset: number;
+  limit: number;
 };
 
-export default function blog({ blog }) {
+async function getData() {
+  const res = await client.get({
+    endpoint: "blog",
+  });
+  console.log(res);
+
+  return res;
+  // return res as Blogs;
+}
+
+export default async function Home() {
+  // 　const data = await getData()
+  const blogs = await getData();
+
   return (
-    <div>
-      {blog.map((blog) => (
-        <li key={blog.id}>
-          <a href="">{blog.title}</a>
-        </li>
-      ))}
-    </div>
+    <>
+      <div>{blogs.contents[0].title}</div>
+      <div>{blogs.contents[0].createdAt}</div>
+      <div>{blogs.contents[0].publishedAt}</div>
+      {/* <div>{blogs.contents[0].createdAt}</div> */}
+      <div>{parse(blogs.contents[0].body)}</div>
+    </>
   );
 }
+
+// "use client";
+// // import styled from "styled-components";
+// import React from "react";
+// // import Image from "next/image";
+// // import Link from "next/link";
+// import { client } from "../../../libs/client";
+
+// // SSG
+// export const getstaticProps = async () => {
+//   const data = await client.get({ endpoint: "blog" });
+//   console.log(date);
+//   return {
+//     props: {
+//       blog: data.contents,
+//     },
+//   };
+// };
+
+// export default function blog({ blogs }) {
+//   return (
+//     <div>
+//       {blogs.map((blog) => (
+//         <li key={blog.id}>
+//           {" "}
+//           <a href="">{blog.title}</a>{" "}
+//         </li>
+//       ))}
+//     </div>
+//   );
+// }
