8d317555a4e0c486b826fe78a2f38c6876af742e
shrinemtg <shrine.mtg@gmail.com>
2023-06-19 06:58:55 +0900

    mainの編集２カラムの確認

--- src/app/Header/header.tsx
+++ src/app/Header/header.tsx
@@ -1,7 +1,54 @@
+import styled from "styled-components";
+
 export default function Header() {
   return (
     <header>
-      <h1>My Style Life</h1>
+      <Title>
+        <h1>My Style Life</h1>
+      </Title>
+      <NavContainer>
+        <nav>
+          <ul>
+            <Licontainer>
+              <a>Style</a>
+              <p>スタイル</p>
+            </Licontainer>
+            <Licontainer>
+              <a>Learn</a>
+              <p>学ぶ</p>
+            </Licontainer>
+            <Licontainer>
+              <a>Purchasing</a>
+              <p>購入</p>
+            </Licontainer>
+          </ul>
+        </nav>
+      </NavContainer>
     </header>
   );
 }
+const Title = styled.div`
+  text-align: center;
+  margin: 30px;
+`;
+const Licontainer = styled.li`
+  list-style: none;
+  text-decoration: none;
+  float: left;
+  overflow: hidden;
+  margin: 10px 30px;
+  margin-bottom: solid 1px;
+  > a {
+    font-size: 24px;
+  }
+  > p {
+    font-size: 14px;
+  }
+`;
+
+const NavContainer = styled.div`
+  text-align: center;
+  justify-content: center;
+  display: flex;
+  /* color: #c0a9da; */
+`;
--- src/app/Main/main.tsx
+++ src/app/Main/main.tsx
@@ -1,13 +1,545 @@
 "use client";
-// import styled from "styled-components";
+import styled from "styled-components";
 // import React from "react";
-// import Image from "next/image";
+import Image from "next/image";
 // import Link from "next/link";
-import { client } from "../../../libs/client";
+// import { client } from "../../../libs/client";
 
 export default function Main() {
-  return <h1>Pic UP</h1>;
+  const [width, height] = [208, 152];
+  return (
+    <>
+      <div>
+        <p>Season</p>
+        <h1>Pick up</h1>
+      </div>
+      <WarapContainer>
+        <ListContainer>
+          <ImageContainer>
+            <Image
+              src="/Imgs/removeーbg/dorsena-bg.png"
+              alt=""
+              width={width}
+              height={height}
+            />
+            <p>2023.06.06</p>
+            <p>
+              ドラセナ・マッサンゲアナ
+              <br />
+              別名、幸福の木とも言われている。
+            </p>
+            <Button>
+              <Image
+                src="/Imgs/removeーbg/good.png"
+                alt=""
+                width={18}
+                height={18}
+              />
+              <Image
+                src="/Imgs/removeーbg/bad.png"
+                alt=""
+                width={18}
+                height={18}
+              />
+            </Button>
+            <Tag>
+              <Image
+                src="/Imgs/removeーbg/tag.png"
+                alt=""
+                width={18}
+                height={18}
+              />
+              <p>タグ</p>
+            </Tag>
+            <Name>
+              <Image
+                src="/Imgs/removeーbg/user.png"
+                alt=""
+                width={18}
+                height={18}
+              />
+              <p>タグ</p>
+            </Name>
+          </ImageContainer>
+          <ImageContainer>
+            <Image
+              src="/Imgs/removeーbg/Pachira-bg.png"
+              alt=""
+              width={width}
+              height={height}
+            />
+            <p>2023.06.06</p>
+            <p>
+              パキラ
+              <br />
+              葉が大きくて丈夫、上に伸びやすい
+            </p>
+            <Button>
+              <Image
+                src="/Imgs/removeーbg/good.png"
+                alt=""
+                width={18}
+                height={18}
+              />
+              <Image
+                src="/Imgs/removeーbg/bad.png"
+                alt=""
+                width={18}
+                height={18}
+              />
+            </Button>
+            <Tag>
+              <Image
+                src="/Imgs/removeーbg/tag.png"
+                alt=""
+                width={18}
+                height={18}
+              />
+              <p>タグ</p>
+            </Tag>
+            <Name>
+              <Image
+                src="/Imgs/removeーbg/user.png"
+                alt=""
+                width={18}
+                height={18}
+              />
+              <p>タグ</p>
+            </Name>
+          </ImageContainer>
+          <ImageContainer>
+            <Image
+              src="/Imgs/removeーbg/kinebosi-bg.png"
+              alt=""
+              width={width}
+              height={height}
+            />
+            <p>2023.06.06</p>
+            <p>
+              キンエボシ
+              <br />
+              烏帽子のような形から名付けられた。
+            </p>
+            <Button>
+              <Image
+                src="/Imgs/removeーbg/good.png"
+                alt=""
+                width={18}
+                height={18}
+              />
+              <Image
+                src="/Imgs/removeーbg/bad.png"
+                alt=""
+                width={18}
+                height={18}
+              />
+            </Button>
+            <Tag>
+              <Image
+                src="/Imgs/removeーbg/tag.png"
+                alt=""
+                width={18}
+                height={18}
+              />
+              <p>タグ</p>
+            </Tag>
+            <Name>
+              <Image
+                src="/Imgs/removeーbg/user.png"
+                alt=""
+                width={18}
+                height={18}
+              />
+              <p>タグ</p>
+            </Name>
+          </ImageContainer>
+          <ImageContainer>
+            <Image
+              src="/Imgs/removeーbg/monsutera-bg.png"
+              alt=""
+              width={width}
+              height={height}
+            />
+            <p>2023.06.06</p>
+            <p>
+              モンステラ
+              <br />
+              別名、幸福の木とも言われている。
+            </p>
+            <Button>
+              <Image
+                src="/Imgs/removeーbg/good.png"
+                alt=""
+                width={18}
+                height={18}
+              />
+              <Image
+                src="/Imgs/removeーbg/bad.png"
+                alt=""
+                width={18}
+                height={18}
+              />
+            </Button>
+            <Tag>
+              <Image
+                src="/Imgs/removeーbg/tag.png"
+                alt=""
+                width={18}
+                height={18}
+              />
+              <p>タグ</p>
+            </Tag>
+            <Name>
+              <Image
+                src="/Imgs/removeーbg/user.png"
+                alt=""
+                width={18}
+                height={18}
+              />
+              <p>タグ</p>
+            </Name>
+          </ImageContainer>
+          <ImageContainer>
+            <Image
+              src="/Imgs/removeーbg/haorucha-bg.png"
+              alt=""
+              width={width}
+              height={height}
+            />
+            <p>2023.06.06</p>
+            <p>
+              ハオルチア
+              <br />
+              別名、幸福の木とも言われている。
+            </p>
+            <Button>
+              <Image
+                src="/Imgs/removeーbg/good.png"
+                alt=""
+                width={18}
+                height={18}
+              />
+              <Image
+                src="/Imgs/removeーbg/bad.png"
+                alt=""
+                width={18}
+                height={18}
+              />
+            </Button>
+            <Tag>
+              <Image
+                src="/Imgs/removeーbg/tag.png"
+                alt=""
+                width={18}
+                height={18}
+              />
+              <p>タグ</p>
+            </Tag>
+            <Name>
+              <Image
+                src="/Imgs/removeーbg/user.png"
+                alt=""
+                width={18}
+                height={18}
+              />
+              <p>タグ</p>
+            </Name>
+          </ImageContainer>
+          <ImageContainer>
+            <Image
+              src="/Imgs/removeーbg/potos-bg.png"
+              alt=""
+              width={width}
+              height={height}
+            />
+            <p>2023.06.06</p>
+            <p>
+              ポトス
+              <br />
+              和名「オウゴンカズラ」
+            </p>
+            <Button>
+              <Image
+                src="/Imgs/removeーbg/good.png"
+                alt=""
+                width={18}
+                height={18}
+              />
+              <Image
+                src="/Imgs/removeーbg/bad.png"
+                alt=""
+                width={18}
+                height={18}
+              />
+            </Button>
+            <Tag>
+              <Image
+                src="/Imgs/removeーbg/tag.png"
+                alt=""
+                width={18}
+                height={18}
+              />
+              <p>タグ</p>
+            </Tag>
+            <Name>
+              <Image
+                src="/Imgs/removeーbg/user.png"
+                alt=""
+                width={18}
+                height={18}
+              />
+              <p>タグ</p>
+            </Name>
+          </ImageContainer>
+          <ImageContainer>
+            <Image
+              src="/Imgs/removeーbg/chimoran-bg.png"
+              alt=""
+              width={width}
+              height={height}
+            />
+            <p>2023.06.06</p>
+            <p>
+              メキシコチモラン
+              <br />
+              乾燥に強く水やりしなくても元気に育つ。
+            </p>
+            <Button>
+              <Image
+                src="/Imgs/removeーbg/good.png"
+                alt=""
+                width={18}
+                height={18}
+              />
+              <Image
+                src="/Imgs/removeーbg/bad.png"
+                alt=""
+                width={18}
+                height={18}
+              />
+            </Button>
+            <Tag>
+              <Image
+                src="/Imgs/removeーbg/tag.png"
+                alt=""
+                width={18}
+                height={18}
+              />
+              <p>タグ</p>
+            </Tag>
+            <Name>
+              <Image
+                src="/Imgs/removeーbg/user.png"
+                alt=""
+                width={18}
+                height={18}
+              />
+              <p>タグ</p>
+            </Name>
+          </ImageContainer>
+          <ImageContainer>
+            <Image
+              src="/Imgs/removeーbg/augusta-bg.png"
+              alt=""
+              width={width}
+              height={height}
+            />
+            <p>2023.06.06</p>
+            <p>
+              オーガスタ
+              <br />
+              バナナの葉のような大きな葉を持ち南国の雰囲気が漂う植物。
+            </p>
+            <Button>
+              <Image
+                src="/Imgs/removeーbg/good.png"
+                alt=""
+                width={18}
+                height={18}
+              />
+              <Image
+                src="/Imgs/removeーbg/bad.png"
+                alt=""
+                width={18}
+                height={18}
+              />
+            </Button>
+            <Tag>
+              <Image
+                src="/Imgs/removeーbg/tag.png"
+                alt=""
+                width={18}
+                height={18}
+              />
+              <p>タグ</p>
+            </Tag>
+            <Name>
+              <Image
+                src="/Imgs/removeーbg/user.png"
+                alt=""
+                width={18}
+                height={18}
+              />
+              <p>タグ</p>
+            </Name>
+          </ImageContainer>
+        </ListContainer>
+        {/*  */}
+        {/*  */}
+        <SideContainer>
+          <FormContainer>
+            <Image
+              src="/Imgs/removeーbg/search.png"
+              alt=""
+              width={18}
+              height={18}
+            />
+            <form id="form4" action="自分のサイトURL" method="get">
+              <input id="sbox4" name="s" type="text" placeholder="To text" />
+            </form>
+          </FormContainer>
+          <div>
+            <HostContainer>
+              <Image
+                src="/Imgs/removeーbg/hiyoko-bg.png"
+                alt=""
+                width={200}
+                height={200}
+              />
+              <h2>Mitsugu Sato</h2>
+              <p>
+                二級建築士
+                <br /> ホームアドバイザー
+                <br />{" "}
+                緑のある暮らしをコンセプトにリビングや書斎に置ける観葉植物を食介していこうと思います。
+              </p>
+            </HostContainer>
+            <h2>カテゴリー</h2>
+            <p>
+              観葉植物 ()
+              <br />
+              エアープランツ ()
+              <br />
+              チランジア ()
+              <br />
+              ストレプトフィラ ()
+              <br />
+              イオナンタ ()
+              <br />
+              フィロデンドロン ()
+              <br />
+              フィカス ()
+              <br />蘭 ()
+              <br />
+              庭木、花木 ()
+              <br />
+              ブラキキトン ()
+              <br />
+              セローム ()
+            </p>
+            <h2>タグ</h2>
+            <Tag>
+              <Image
+                src="/Imgs/removeーbg/tag.png"
+                alt=""
+                width={18}
+                height={18}
+              />
+              <p>タグ</p>
+              <Image
+                src="/Imgs/removeーbg/tag.png"
+                alt=""
+                width={18}
+                height={18}
+              />
+              <p>タグ</p>
+              <Image
+                src="/Imgs/removeーbg/tag.png"
+                alt=""
+                width={18}
+                height={18}
+              />
+              <p>タグ</p>
+              <Image
+                src="/Imgs/removeーbg/tag.png"
+                alt=""
+                width={18}
+                height={18}
+              />
+              <p>タグ</p>
+            </Tag>
+          </div>
+        </SideContainer>
+      </WarapContainer>
+    </>
+  );
 }
+
+const HostContainer = styled.div`
+  margin-top: 56px;
+  > h2 {
+    text-align: center;
+    border-bottom: 1px solid;
+    width: 200px;
+    margin-bottom: 20px;
+  }
+`;
+const WarapContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  max-width: 1120px; /* 最大幅を1080pxに設定 */
+  margin: 0 auto; /* 左右の余白を自動で設定 */
+`;
+const FormContainer = styled.form`
+  display: flex;
+  align-items: center;
+  #form4 {
+    position: relative;
+    max-width: 300px;
+    margin-bottom: 20px;
+  }
+  #sbox4 {
+    height: 50px;
+    padding: 0 10px;
+    position: absolute;
+    left: 0;
+    top: 0;
+    border-radius: 25px;
+    outline: 0;
+    background: #eee;
+  }
+`;
+const SideContainer = styled.div`
+  width: 208px;
+  margin: 0 auto;
+`;
+const Button = styled.button`
+<a href=""></a>`;
+const Tag = styled.div`
+  display: flex;
+`;
+const Name = styled.div`
+  display: flex;
+`;
+
+const ImageContainer = styled.div`
+  width: 208px;
+  > img {
+    box-shadow: 1px 1px 8px rgba(0, 0, 0, 0.8);
+  }
+`;
+
+const ListContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  margin: auto;
+  gap: 32px;
+
+  @media screen and (max-width: 748px) {
+    margin: 0;
+  }
+`;
 
 // // SSG
 // export const getstaticProps = async () => {
